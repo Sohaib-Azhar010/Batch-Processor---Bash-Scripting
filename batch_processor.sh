dfde9471ad3f4a4874d1cb0b3cf18f752b13d648
Sohaib Azhar <sohaibazhar04@gmail.com>
2025-08-21 13:51:04 +0500

    Question added

--- batch_processor.sh
+++ batch_processor.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Assignment 3: Batch Processor
+# Write a bash script that:
+# Asks the user for a folder path.
+# Finds all .txt files in that folder.
+# Copies them all to the Desktop.
+# Appends today’s date (YYYY-MM-DD) to each copied file’s name.
+# Example: report.txt → report_2025-08-20.txt.
+# Deletes all the original .txt files from the given folder.
+# : Show a summary at the end like:
+# Processed 5 files. Copied to Desktop and deleted originals.
+
+
 read -p "Enter the folder path: " folder
 
 if [ ! -d "$folder" ]; then
